3eeac8dc25139051b370f8c28faf78ed2978c2c9
Makman2 <makmanx64@gmail.com>
2018-07-11 22:11:55 +0200

    WIP

--- healthserver.go
+++ healthserver.go
@@ -18,12 +18,6 @@ type Check interface {
 	Check() error
 }
 
-/*
-func (hs *HealthServer) AddCheck(check Check, endpoint string) {
-	hs.checks[endpoint] = append(hs.checks[endpoint], check)
-}
-*/
-
 func check(checks []Check, response http.ResponseWriter) {
 	for _, check := range checks {
 		err := check.Check()
@@ -40,7 +34,7 @@ func (hs *HealthServer) Start() {
 	handler := http.NewServeMux()
 
 	for _, endpoint := range hs.Endpoints {
-		handler.HandleFunc(endpoint.Name, func(response http.ResponseWriter, request *http.Request) {
+		handler.HandleFunc("/"+endpoint.Name, func(response http.ResponseWriter, request *http.Request) {
 			check(endpoint.Checks, response)
 		})
 	}
